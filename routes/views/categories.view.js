a1aa851a5e4160ed5abab5fd53f7a97efb238ece
MaimulaK <maimulakonstantin@gmail.com>
2024-02-22 18:55:28 +0300

    Release categories with Id

--- routes/views/categories.view.js
+++ routes/views/categories.view.js
@@ -1,16 +1,34 @@
 const router = require('express').Router();
 const { Category } = require('../../db/models');
 const CategoriesPage = require('../../components/CategoriesPage');
+const CategoryId = require('../../components/CategoryId');
 
-router.get('/', async (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
-    const categories = await Category.findAll();
-    const document = res.renderComponent(CategoriesPage, { title:'cat', categories });
+    const { id } = req.params;
+    console.log(id);
+    const category = await Category.findOne({ where: { id } });
+    const document = res.renderComponent(CategoryId, { category });
     res.send(document);
   } catch ({ message }) {
     res.status(500).json({ error: message });
   }
 });
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll();
+    const document = res.renderComponent(CategoriesPage, {
+      title: 'cat',
+      categories,
+    });
+    res.send(document);
+  } catch ({ message }) {
+    res.status(500).json({ error: message });
+  }
+});
+
+
+module.exports = router;
 
 // router.get('/category', async (req, res) => {
 //   try {
