6576fee7d26f7e861244f1e766988b33a9bbc86e
ZakharchenkoV <zvk096@gmail.com>
2024-02-22 17:41:23 +0300

    registration rout

--- routes/index.router.js
+++ routes/index.router.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 //views подключения
 // const mainRouter = require('./main.router');
 const classRouter = require('./views/categories.view');
+const authRouter = require('../routes/views/auth.view');
 
 //rout views
-router.use('./categories', classRouter);
+router.use('/categories', classRouter);
+router.use('/registration', authRouter);
 
 module.exports = router;
