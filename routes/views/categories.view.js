83921ab468ab45a1a32aebecfde85e687a6355af
MaimulaK <maimulakonstantin@gmail.com>
2024-02-23 09:47:24 +0300

    release with answers

--- routes/views/categories.view.js
+++ routes/views/categories.view.js
@@ -2,13 +2,24 @@ const router = require('express').Router();
 const { Category } = require('../../db/models');
 const CategoriesPage = require('../../components/CategoriesPage');
 const CategoryId = require('../../components/CategoryId');
+const { Quiz } = require('../../db/models');
+const { Otvet } = require('../../db/models');
 
 router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
     console.log(id);
     const category = await Category.findOne({ where: { id } });
-    const document = res.renderComponent(CategoryId, { category });
+    const quize = await Quiz.findOne({
+      where: { id },
+      include: { model: Otvet },
+    });
+    console.log(quize);
+    // const ott = await Otvet.findAll({ where: { id } });
+    const document = res.renderComponent(CategoryId, {
+      quize,
+      category,
+    });
     res.send(document);
   } catch ({ message }) {
     res.status(500).json({ error: message });
@@ -26,7 +37,6 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: message });
   }
 });
-
 
 module.exports = router;
 
